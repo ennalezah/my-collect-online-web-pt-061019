43f3491bca97b9cb3b8ae0878dfd27a6e769f429
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-18 08:50:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   i = 0
   new_collection = []
   
-  while i < array.length 
-    new collection << yield collection[i]
+  while i < collection.length 
+    new_collection << yield collection[i]
     i += 1
   end 
   new_collection
